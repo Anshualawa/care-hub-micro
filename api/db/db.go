afd9c68c49c24a89d8552c499bc1bca08bb23fec
Pappu Alawa <95533048+Anshualawa@users.noreply.github.com>
2025-04-22 12:16:38 +0530

    Update db.go for remove pc

--- api/db/db.go
+++ api/db/db.go
@@ -12,7 +12,7 @@ var DB *sql.DB
 func InitDB() error {
 	// Replace these with your actual MySQL credentials
 	username := "root"
-	password := "root"
+	password := ""
 	host := "localhost"
 	port := "3306"
 	dbname := "carehub"
